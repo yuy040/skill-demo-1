c5a46cde0e3a17c2c00d14b310273d648c82f83c
yuy040 <yuy040@ucsd.edu>
2022-05-10 15:59:42 -0700

    Skill Demo

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo {
-
+    public static int multiply(int m, int n) {
+        return m * n;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+
+    @Test
+    public void multiplication() {
+    assertEquals(1, SkillDemo.multiply(2, 2));
+    }
+}
