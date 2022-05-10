d819b614e598764cecebda690ae9fcb5827fe04b
yuy040 <yuy040@ucsd.edu>
2022-05-10 16:02:09 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,6 +5,6 @@ public class SkillDemoTest {
 
     @Test
     public void multiplication() {
-    assertEquals(1, SkillDemo.multiply(2, 2));
+    assertEquals(4, SkillDemo.multiply(2, 2));
     }
 }
